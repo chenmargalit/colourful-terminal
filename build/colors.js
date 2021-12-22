dedbf418266a4c7b4e6853b7517cc1a98dfd8034
chenmargalit <chendevbetter@gmail.com>
2021-12-22 05:54:12 +0200

    fix bug

--- build/colors.js
+++ build/colors.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mapColors_1 = __importDefault(require("./mapColors"));
-const printColor = (colorName, itemToPrint) => {
+const printColor = (colorName, itemToConsole) => {
     const colorNumber = (0, mapColors_1.default)(colorName.toLowerCase());
-    return console.log(`\x1b[${colorNumber}m%s\x1b[0m`, `${itemToPrint}`);
+    return console.log(`\x1b[${colorNumber}`, itemToConsole, '\x1b[0m');
 };
 exports.default = printColor;
