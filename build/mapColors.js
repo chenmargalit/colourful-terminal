7a159d592da2bec4533b1ef047480964ab4c0bba
chenmargalit <chendevbetter@gmail.com>
2021-12-23 21:06:06 +0200

    add green color

--- build/mapColors.js
+++ build/mapColors.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const colorsMapper = (colorName) => {
     const colorsDictionary = {
         cyan: 36,
+        green: 32
     };
     const colorsNumber = colorsDictionary[colorName];
     return colorsNumber;
